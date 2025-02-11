797609126427dcbff0589ae8b0e7f2c60fde3cd4
Maria Eduarda <146599135+Mary606@users.noreply.github.com>
2025-02-11 16:42:43 -0300

    'terceira'

---
+++ quiz.js
@@ -0,0 +1,58 @@
+let question=[
+    {
+        numb: 1,
+        question: "Quem é esse personagem?",
+        answer: "a- Naruto",
+        options: [
+            "a- Naruto Usumaki",
+            "b- Goku",
+            "c- Asta",
+            "d- Eren Yager"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "De onde é essa logo?",
+        answer: "",
+        options: [
+            "a- Fairy tail",
+            "b- Bleach",
+            "c- Assassination Classroom",
+            "d- One piece"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quem é o Pai do luffy?",
+        answer: "",
+        options: [
+            "a- Barba negra",
+            "b- Gold D. Roger",
+            "c- Garper",
+            "d- Dragon"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quantos eps tem Gintama",
+        answer: "",
+        options: [
+            "a- Barba negra",
+            "b- Gold D. Roger",
+            "c- Garper",
+            "d- Dragon"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quem é o par romantico desse personagem?",
+        answer: "",
+        options: [
+            "a- Barba negra",
+            "b- Gold D. Roger",
+            "c- Garper",
+            "d- Dragon"
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBotao = document.querySelector(".sair");
 const main = document.querySelector(".inicio");
 const continuarBotao = document.querySelector(".continuar");
 const quizAnime = document.querySelector(".quiz");
+const quizSecao= document.querySelector(".quiz-secao");
 
 
 const audio = new Audio();
@@ -24,8 +25,27 @@ continuarBotao.onclick = () => {
   quizAnime.classList.add("active");
   infoBox.classList.remove("active");
   main.classList.remove("active");
+  quizSecao.classList.add("active");
+
+  showQuestions(0);
 
   audio
     .play()
     .catch((error) => console.log("Erro ao tentar reproduzir o áudio:", error));
 };
+
+let questionCont = 0;
+
+const proxBotao = document.querySelector('.proximo');
+
+proxBotao.onclick = () => {
+  questionCont++;
+  showQuestions(questionCont);
+}
+ 
+
+function showQuestions (index){
+  const questionTexto = document.querySelector('.perguntas');
+  questionTexto.textContent = `${questions[index].numb}. ${questions[index].question}`;
+}
+
